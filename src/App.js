9e063b4709a560b683e85a74c79fc15d4f57dad2
Scynes <dustin.greyfield@gmail.com>
2023-02-01 17:43:14 -0500

    complete: added site navigation routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavigationBar from "./components/navigation";
+import Landing from "./pages/Landing"
 
 const App = () => {
-
     return (
-        <>
-
-        </>
+        <BrowserRouter>
+            <NavigationBar />
+            <Routes>
+                <Route path="/" element={ <Landing /> }/>
+                <Route path="/skills" element={ <Landing /> }/>
+                <Route path="/projects" element={ <Landing /> }/>
+                <Route path="/resume" element={ <Landing /> }/>
+                <Route path="/contact" element={ <Landing /> }/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/navigation/NavigationBar.js
@@ -0,0 +1,11 @@
+const NavigationBar = () => {
+    return(
+        <nav>
+            <div>
+                Hello
+            </div>
+        </nav>
+    )
+}
+
+export default NavigationBar;
