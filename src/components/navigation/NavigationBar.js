b4637950c1bc976739797b76da0b3d5ec991fa30
Scynes <dustin.greyfield@gmail.com>
2023-02-02 13:22:45 -0500

    wip: hamburger menu for mobile viewport on navigation

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -4,20 +4,25 @@ import logo from '../../assets/images/logo.svg'
 
 const NavigationBar = () => {
     return(
-        <nav id="navigation-bar" className="flex align-center justify-end">
+        <nav id={'navigation-bar'} className={'flex align-center'}>
             <img src={logo} alt="" />
-            <NavLink className={'nav-item fc-primary'} to={''}>
-                <h3>Home</h3>
-            </NavLink>
-            <NavLink className={'nav-item fc-primary'} to={'skills'}>
-                <h3>Skills</h3>
-            </NavLink>
-            <NavLink className={'nav-item fc-primary'} to={'resume'}>
-                <h3>Résumé</h3>
-            </NavLink>
-            <NavLink className={'nav-item fc-primary'} to={'contact'}>
-                <h3>Contact</h3>
-            </NavLink>
+            <button className={'hamburger pointer'}>
+                <i class="fa-solid fa-bars"></i>
+            </button>
+            <div id={'nav-items'} className={'flex'}>
+                <NavLink className={'nav-item fc-primary'} to={''}>
+                    <h3>Home</h3>
+                </NavLink>
+                <NavLink className={'nav-item fc-primary'} to={'skills'}>
+                    <h3>Skills</h3>
+                </NavLink>
+                <NavLink className={'nav-item fc-primary'} to={'resume'}>
+                    <h3>Résumé</h3>
+                </NavLink>
+                <NavLink className={'nav-item fc-primary'} to={'contact'}>
+                    <h3>Contact</h3>
+                </NavLink>
+            </div>
         </nav>  
     )
 }
