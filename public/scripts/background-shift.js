cd8b0e7b65360d0ed2989971196704f9cd70be5a
Scynes <dustin.greyfield@gmail.com>
2023-02-02 16:18:17 -0500

    complete: footer

--- public/scripts/background-shift.js
+++ public/scripts/background-shift.js
@@ -5,7 +5,7 @@ const shiftBackground = event => {
     const MOUSE_X = event.clientX / window.innerWidth;
     const MOUSE_Y = event.clientY / window.innerHeight;
 
-    BODY.style.backgroundPosition = `${10 * MOUSE_X}% ${10 * MOUSE_Y}%`
+    BODY.style.backgroundPosition = `${5 * MOUSE_X}% ${5 * MOUSE_Y}%`
 }
 
 if (!/Mobi|Android/i.test(navigator.userAgent)) {
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,15 @@
 const Footer = () => {
     return (
         <footer className={'flex align-center justify-center fc-primary'}>
-            <i className={'fa-brands fa-discord fa-3x'}></i>
-            <i className={'fa-brands fa-github fa-3x'}></i>
-            <i className={'fa-brands fa-linkedin fa-3x'}></i>
+            <a className={'flex align-center justify-center pointer'} href="http://google.com">
+                <i className={'fa-brands fa-discord fa-3x'}></i>
+            </a>
+            <a className={'flex align-center justify-center pointer'} href="https://github.com/Scynes">
+                <i className={'fa-brands fa-github fa-3x'}></i>
+            </a>
+            <a className={'flex align-center justify-center pointer'} href="https://www.linkedin.com/in/dustin-greyfield-web-developer/">
+                <i className={'fa-brands fa-linkedin fa-3x'}></i>
+            </a>
         </footer>
     )
 }
