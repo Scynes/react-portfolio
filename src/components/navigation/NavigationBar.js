6164627ef97ebd87c59e177d3a6620baf9663727
Scynes <dustin.greyfield@gmail.com>
2023-02-01 21:10:29 -0500

    complete: navigation bar

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -1,10 +1,22 @@
+import { NavLink } from "react-router-dom";
+
 const NavigationBar = () => {
     return(
-        <nav>
-            <div>
-                Hello
-            </div>
-        </nav>
+        <nav id="navigation-bar" className="flex align-center justify-end">
+            <h1 className={'fc-primary'}>DUSTIN</h1>
+            <NavLink className={'nav-item fc-primary'} to={''}>
+                <h3>Home</h3>
+            </NavLink>
+            <NavLink className={'nav-item fc-primary'} to={'skills'}>
+                <h3>Skills</h3>
+            </NavLink>
+            <NavLink className={'nav-item fc-primary'} to={'resume'}>
+                <h3>Résumé</h3>
+            </NavLink>
+            <NavLink className={'nav-item fc-primary'} to={'contact'}>
+                <h3>Contact</h3>
+            </NavLink>
+        </nav>  
     )
 }
 
