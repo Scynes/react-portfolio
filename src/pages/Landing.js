6b26a688f6d045ce4d3355f5b0fe2e5003b5fc36
Scynes <dustin.greyfield@gmail.com>
2023-02-03 15:22:24 -0500

    complete: added portfolio pic svg rendering on landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,11 +1,25 @@
 import '../assets/typer.css'
 
+import portfolioPicture from '../assets/images/default_portfolio_pic.png'
+
 const Landing = () => {
 
     return (
-        <div id={'landing-page-wrapper'} className={'flex column align-center justify-center'}>
-            <div className={'about-container fc-primary'}>
-                <p className={'typer'}></p>
+        <div id={'landing-page-wrapper'} className={'justify-center'}>
+            <div className="flex justify-center">
+                <svg id={'portfolio-picture'} viewBox="0 0 160 160" width="25rem">
+                    <image xlinkHref={portfolioPicture} x="35%" y="35%" width="30%" clipPath={'inset(0% round 15px)'}/>
+                    <g transform=" matrix(0.866, -0.5, 0.25, 0.433, 80, 80)">
+                        <path d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z" fill="#fff">
+                            <animateTransform attributeName="transform" type="rotate" from="360 0 0" to="0 0 0" dur="1s" repeatCount="indefinite" />
+                        </path>
+                    </g>
+                </svg>
+            </div>
+            <div className={'about-container flex justify-center fc-primary'}>
+                <div>
+                    <p className={'typer'}></p>
+                </div>
             </div>
         </div>
     )
