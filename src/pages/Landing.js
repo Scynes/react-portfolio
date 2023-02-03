5e5f7de66714307c954e6b501bf857fb78eecc1e
Scynes <dustin.greyfield@gmail.com>
2023-02-03 15:39:06 -0500

    complete: mobile/desktop landing page split

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,9 +5,9 @@ import portfolioPicture from '../assets/images/default_portfolio_pic.png'
 const Landing = () => {
 
     return (
-        <div id={'landing-page-wrapper'} className={'justify-center'}>
+        <div id={'landing-page-wrapper'} className={'grid justify-center'}>
             <div className="flex justify-center">
-                <svg id={'portfolio-picture'} viewBox="0 0 160 160" width="25rem">
+                <svg id={'portfolio-picture'} viewBox="0 0 160 160" width="40rem">
                     <image xlinkHref={portfolioPicture} x="35%" y="35%" width="30%" clipPath={'inset(0% round 15px)'}/>
                     <g transform=" matrix(0.866, -0.5, 0.25, 0.433, 80, 80)">
                         <path d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z" fill="#fff">
@@ -16,7 +16,7 @@ const Landing = () => {
                     </g>
                 </svg>
             </div>
-            <div className={'about-container flex justify-center fc-primary'}>
+            <div className={'about-container flex align-center fc-primary'}>
                 <div>
                     <p className={'typer'}></p>
                 </div>
