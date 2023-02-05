206d8efb04ef61ceb108b932d6a19c1bff8f3f0a
Scynes <dustin.greyfield@gmail.com>
2023-02-05 12:00:13 -0500

    wip: Projects page flex layout

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,12 @@
 const Projects = () => {
     
     return (
-        <div id={'projects-container'} className={'flex column align-center justify-center width-100 height-100'}>
+        <div id={'projects-container'} className={'flex relative column align-center justify-center width-100 height-100'}>
             <div className="projects-background width-100 height-100"></div>
-            <div id="projects-flex" className="flex">
+            <h1 className={'relative fc-primary'}>Projects</h1>
+            <div id="projects-flex" className="flex relative justify-center">
+                <div className="project fc-primary">lore</div>
+                <div className="project fc-primary">lore</div>
                 <div className="project fc-primary">lore</div>
                 <div className="project fc-primary">lore</div>
                 <div className="project fc-primary">lore</div>
